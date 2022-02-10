1887cb5b0a490591fb74d8c71cc4979928689e74
Bukonla <bukonla.osokoya@gmail.com>
2022-02-10 21:26:37 +0100

    my first commit

--- ISP.py
+++ ISP.py
@@ -5,25 +5,20 @@ img = Image.open("isp.png")
 
 st.image(img, width=500)
 
-st.title("Internet Service Provider Survey")
+st.title("Internet Service Providers Survey")
 
-st.header("How good is your internet?")
-
-st.subheader("please answer the following questions")
+st.subheader("Please answer the following questions")
 
 st.text("How long have you been using the internet?")
-
-st.success("Success")
-
-if st.checkbox("Select/Unselect"):
-    st.text("Selection Ready")
     
-status = st.radio("Select internet: ", ('0-2 years', 'Above 2 years'))
+status = st.radio("Select year: ", ('0-2 years', 'Above 2 years'))
 
 if status == '0-2 years':
     st.success("0-2 years")
 else:
     st.success("Above 2 years")   
+    
+st.text("Which internet service provider do you use?")    
     
 internet = st.selectbox("Select internet: ",
                      ['MTN', 'GLO', 'Airtel'])
@@ -31,12 +26,7 @@ internet = st.selectbox("Select internet: ",
 st.write("You have selected: ", internet)
                   
                   
-st.text("Are you satisfied with your current internet service provider?")
-
-st.success("Success")
-
-if st.checkbox("Select/Unselect"):
-    st.text("Selection Ready")
+st.text("Would you recommend your internet service provider to your friend?")
     
 status = st.radio("Select answer: ", ('YES', 'NO'))
 
@@ -44,22 +34,25 @@ if (status == 'YES'):
     st.success("YES")
 else:
     st.success("NO")   
+
+st.text("How fast is your internet?")
     
 speed = st.selectbox("Select speed: ",
                      ['Slow', 'Fast', 'Very Fast'])
 
-st.write("You have selected: ", speed)               
-                                  
+st.write("You have selected: ", speed)   
 
-name = st.text_input("Enter Your name", "Type Here ...")
+level = st.slider("How satisfied are you with your internet service provider on a scale of 1-5?", 1, 5)
+
+st.text('Selected: {}'.format(level))
+
+name = st.text_input("Enter Your name", "Type Here ...")                                  
 
 if(st.button('Submit')):
     result = name.title()
     st.success(result)
     
-level = st.slider("How Satisfied are you on a Scale of 5?", 1, 5)
 
-st.text('Selected: {}'.format(level))
 
 
  
